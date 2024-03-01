d64ac9e55a173a1d7c94560fab6b1bad06ae2cb3
Ben <benward151@gmail.com>
2024-03-01 20:49:25 +0100

    destinations dropdown

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ function toggleMenu() {
   navigationItems.classList.toggle('nav-open');
 }
 
+//destinations dropdown
+function destinationsDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 //accordion
 document.addEventListener("DOMContentLoaded", function() {
